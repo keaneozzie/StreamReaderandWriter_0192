ff84c81eb3a1b538198cb45aadda51d47d4c0c22
keaneozzie <ozziedharma897@gmail.com>
2026-06-01 18:38:14 +0700

    menambahkan array data sebagai bahan percobaan

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -9,6 +9,9 @@ int main()
     cout << "Awal program" << endl; //penanda 1
     try
     {
+        //deklarasi array data dengan panjang index 3
+        array<int, 3> data = { 1, 2, 3 };
+
         
     return 0;
 }

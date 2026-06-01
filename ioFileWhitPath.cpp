a169c07cd0616567c2689fb24b54af48315462b2
keaneozzie <ozziedharma897@gmail.com>
2026-06-01 18:50:12 +0700

    menambahkan fitur menulis data

--- ioFileWhitPath.cpp
+++ ioFileWhitPath.cpp
@@ -13,4 +13,9 @@ int main()
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
 
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
     

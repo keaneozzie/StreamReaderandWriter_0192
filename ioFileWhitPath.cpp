a8ea41fae4fa11a4b815a97bfead18cbd99a16f0
keaneozzie <ozziedharma897@gmail.com>
2026-06-01 18:51:47 +0700

    menambahkan fitur membaca

--- ioFileWhitPath.cpp
+++ ioFileWhitPath.cpp
@@ -34,4 +34,13 @@ int main()
 
     }
 
-    
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file dengan ekstensi .txt
+    infile.open(NamaFile + ".txt", ios::in);
+
+   

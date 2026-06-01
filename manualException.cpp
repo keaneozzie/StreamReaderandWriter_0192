e9dc9847f04165a583a5658e223689b2fdf2e5df
keaneozzie <ozziedharma897@gmail.com>
2026-06-01 18:44:26 +0700

    membuat contoh exception manual menggunakan try catch

--- manualException.cpp
+++ manualException.cpp
@@ -7,21 +7,4 @@ int main()
     try
     {
         cout << "Selamat belajar di prodi TI UMY" << endl;
-        //throw 0.5; //melemparkan sebuah integer maka
-        cout << "Pernyataan tidak akan ditampilkan" << endl;
-    }
-
-    //kondisi jika throw yang bertipe data integer
-    catch (int a)
-    {
-        cout << "Pengecualian akan di eksekusi" << endl;
-    }
-
-    //kondisi jika throw bertipe data selain integer
-    catch (...)
-    {
-        cout << "default pengecualian diekseskusi" << endl;
-    }
-
-    return 0;
-}
+        
